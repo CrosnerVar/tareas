611c3918b0dffafcb64d8ce41384a8e1f28f49eb
CrosnerVar <A01202162@itesm.mx>
2016-04-03 20:48:21 -0500

    commit 2

--- pruebagut.playground/Contents.swift
+++ pruebagut.playground/Contents.swift
@@ -2,4 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var rango = 0...100
+for i in rango{
+    if i == 0{
+        print ("# \(i)"  + " Cero!!! ")
+    }else if (i < 41) && (i>29) {
+        print ("# \(i)"  + " Viva Swift!!! ")
+    }else if (i % 5) == 0{
+        print ("# \(i)"  + " Bingo!!! ")
+    }else if i % 2 == 0{
+        print ("# \(i)" + " par!!!" )
+    }else if i % 2 != 0{
+        print ("# \(i)" + " impar!!!" )
+    }
+}
