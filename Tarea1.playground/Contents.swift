ea9d785418e10c215f8df453eb59bb6ff1d647b6
CrosnerVar <A01202162@itesm.mx>
2016-04-17 22:39:23 -0500

    v

--- Tarea1.playground/Contents.swift
+++ Tarea1.playground/Contents.swift
@@ -2,17 +2,42 @@
 
 import UIKit
 
-var rango = 0...100
-for i in rango{
-    if i == 0{
-        print ("# \(i)"  + " Cero!!! ")
-    }else if (i < 41) && (i>29) {
-        print ("# \(i)"  + " Viva Swift!!! ")
-    }else if (i % 5) == 0{
-        print ("# \(i)"  + " Bingo!!! ")
-    }else if i % 2 == 0{
-        print ("# \(i)" + " par!!!" )
-    }else if i % 2 != 0{
-        print ("# \(i)" + " impar!!!" )
+enum Velocidades: Int {
+    
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades ){
+        self = velocidadInicial
     }
 }
+class Auto {
+    var velocidad: Velocidades
+    init(){
+        velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual: Int, velocidadEnCadena: String){
+        switch velocidad {
+            
+        case .Apagado:
+            velocidad = Velocidades.VelocidadBaja
+        case .VelocidadBaja:
+            velocidad = Velocidades.VelocidadMedia
+        case .VelocidadMedia:
+            velocidad = Velocidades.VelocidadAlta
+        case .VelocidadAlta:
+            velocidad = Velocidades.VelocidadMedia
+        }
+        return (velocidad.rawValue,  "\(velocidad)")
+    }
+    
+}
+
+var auto = Auto()
+print (auto.velocidad.rawValue,  (auto.velocidad))
+for index in 1...20{
+    var temp = auto.cambioDeVelocidad()
+    print (temp.0, temp.1)
+}
+
+
